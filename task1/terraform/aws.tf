e658a382e4d8fc00feffd484b9bf020c74408797
shanawar <shanawar680@hotmail.com>
2023-08-28 13:51:52 +0500

    fix ansible

--- task1/terraform/aws.tf
+++ task1/terraform/aws.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "ec2_instance" {
   ami           = "ami-0261755bbcb8c4a84"  # Ubuntu
   instance_type = "t2.medium"
   associate_public_ip_address = true
+  key_name = "jenkins"
   tags = {
     Name = "EC2_INSTANCE_BY_JENKINS"
   }
